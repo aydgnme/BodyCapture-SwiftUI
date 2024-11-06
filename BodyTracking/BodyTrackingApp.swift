3a8e0ac046ef65686804dca1afd50a1f461a4454
ioanaydgn <mertaydogn0@gmail.com>
2024-11-06 17:35:17 +0200

    Initial Commit

---
+++ BodyTracking/BodyTrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  BodyTrackingApp.swift
+//  BodyTracking
+//
+//  Created by Mert Aydoğan on 06.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct BodyTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
